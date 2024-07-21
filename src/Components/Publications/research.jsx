76951c5a7856a9be76f5e59390ac83e8e1872a1a
Priya S Kulkarni <67280246+PriyaSKulkarni@users.noreply.github.com>
2024-07-21 15:24:02 -0500

    Add files via upload

--- src/Components/Publications/research.jsx
+++ src/Components/Publications/research.jsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
-import Papa from "papaparse";
-import "./research.css";
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import './research.css';
 
 const baseURL = import.meta.env.BASE_URL;
 
 const CsvReader = () => {
   const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [sortOrder, setSortOrder] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
   const [selectedField, setSelectedField] = useState([]);
   const [selectedYears, setSelectedYears] = useState([]);
   const [selectedType, setSelectedType] = useState([]);
   const [filtersVisible, setFiltersVisible] = useState(true);
 
   useEffect(() => {
-    fetch(`${baseURL}/research.csv`)
+    fetch(`${baseURL}public/research.csv`)
       .then((response) => response.text())
       .then((csv) => {
         Papa.parse(csv, {
@@ -25,27 +25,33 @@ const CsvReader = () => {
           },
         });
       });
+      const mediaQuery = window.matchMedia('(max-width: 426px)');
+    setFiltersVisible(!mediaQuery.matches); // true for desktop, false for mobile
+
+    // Add event listener for changes in screen size
+    const handleResize = () => {
+      setFiltersVisible(!mediaQuery.matches); // true for desktop, false for mobile
+    };
+
+    mediaQuery.addEventListener('change', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      mediaQuery.removeEventListener('change', handleResize);};
   }, []);
 
   const toggleFiltersVisibility = () => {
-    if (filtersVisible) {
-      setSelectedField([]);
-      setSelectedYears([]);
-      setSelectedType([]);
-    }
-    setFiltersVisible((prevVisible) => !prevVisible);
+      setFiltersVisible((prevVisible) => !prevVisible);
   };
 
   const handleTypeSelect = (type) => {
     let updatedSelectedType = [];
 
-    if (type === "All") {
+    if (type === 'All') {
       updatedSelectedType = [];
     } else {
       if (selectedType.includes(type)) {
-        updatedSelectedType = selectedType.filter(
-          (selected) => selected !== type
-        );
+        updatedSelectedType = selectedType.filter((selected) => selected !== type);
       } else {
         updatedSelectedType = [...selectedType, type];
       }
@@ -56,13 +62,11 @@ const CsvReader = () => {
   const handleFieldSelect = (field) => {
     let updatedSelectedField = [];
 
-    if (field === "All") {
+    if (field === 'All') {
       updatedSelectedField = [];
     } else {
       if (selectedField.includes(field)) {
-        updatedSelectedField = selectedField.filter(
-          (selected) => selected !== field
-        );
+        updatedSelectedField = selectedField.filter((selected) => selected !== field);
       } else {
         updatedSelectedField = [...selectedField, field];
       }
@@ -79,23 +83,15 @@ const CsvReader = () => {
   };
 
   const filteredPapers = data
-    .filter((row) =>
-      row.Title.toLowerCase().includes(searchQuery.toLowerCase())
-    )
-    .filter((row) =>
-      selectedField.length > 0 ? selectedField.includes(row.Field) : true
-    )
-    .filter((row) =>
-      selectedYears.length > 0 ? selectedYears.includes(row.Year) : true
-    )
-    .filter((row) =>
-      selectedType.length > 0 ? selectedType.includes(row.Paper) : true
-    );
+    .filter((row) => row.Title.toLowerCase().includes(searchQuery.toLowerCase()))
+    .filter((row) => (selectedField.length > 0 ? selectedField.includes(row.Field) : true))
+    .filter((row) => (selectedYears.length > 0 ? selectedYears.includes(row.Year) : true))
+    .filter((row) => (selectedType.length > 0 ? selectedType.includes(row.Paper) : true));
 
   const sortedPapers = [...filteredPapers].sort((a, b) => {
-    if (sortOrder === "asc") {
+    if (sortOrder === 'asc') {
       return a.Year.localeCompare(b.Year);
-    } else if (sortOrder === "desc") {
+    } else if (sortOrder === 'desc') {
       return b.Year.localeCompare(a.Year);
     } else {
       return 0; // No sorting applied
@@ -103,9 +99,7 @@ const CsvReader = () => {
   });
 
   const YearFilter = () => {
-    const years = Array.from(new Set(data.map((row) => row.Year))).filter(
-      (year) => year
-    );
+    const years = Array.from(new Set(data.map((row) => row.Year))).filter((year) => year);
 
     return (
       <div className="filter-option">
@@ -115,7 +109,7 @@ const CsvReader = () => {
             type="checkbox"
             value=""
             checked={selectedYears.length === 0}
-            onChange={() => handleYearSelect("")}
+            onChange={() => handleYearSelect('')}
           />
           All
           <span className="checkmark"></span>
@@ -138,9 +132,7 @@ const CsvReader = () => {
 
   const TypeFilter = () => {
     // Get unique fields
-    const uniqueTypes = Array.from(
-      new Set(data.map((row) => row.Paper))
-    ).filter((type) => !/\s/.test(type));
+    const uniqueTypes = Array.from(new Set(data.map((row) => row.Paper))).filter((type) => !/\s/.test(type));
 
     // Sort unique fields by length
     uniqueTypes.sort((a, b) => a.length - b.length);
@@ -153,7 +145,7 @@ const CsvReader = () => {
             type="checkbox"
             value="All"
             checked={selectedType.length === 0}
-            onChange={() => handleTypeSelect("All")}
+            onChange={() => handleTypeSelect('All')}
           />
           All
           <span className="checkmark"></span>
@@ -176,9 +168,7 @@ const CsvReader = () => {
 
   const FieldFilter = () => {
     // Get unique fields
-    const uniqueFields = Array.from(
-      new Set(data.map((row) => row.Field))
-    ).filter((type) => /\s/.test(type));
+    const uniqueFields = Array.from(new Set(data.map((row) => row.Field))).filter((type) => /\s/.test(type));
 
     // Sort unique fields by length
     uniqueFields.sort((a, b) => a.length - b.length);
@@ -191,7 +181,7 @@ const CsvReader = () => {
             type="checkbox"
             value="All"
             checked={selectedField.length === 0}
-            onChange={() => handleFieldSelect("All")}
+            onChange={() => handleFieldSelect('All')}
           />
           All
           <span className="checkmark"></span>
@@ -215,13 +205,9 @@ const CsvReader = () => {
   return (
     <div className="main">
       <div className="filter-toggle" onClick={toggleFiltersVisibility}>
-        {filtersVisible ? (
-          <span className="minus">&ndash;</span>
-        ) : (
-          <span className="plus">+</span>
-        )}
+        {filtersVisible ? <span className="minus">&ndash; Filters</span> : <span className="plus">+ Filters</span>}
       </div>
-      <div className={`left-filters ${filtersVisible ? "show" : "hide"}`}>
+      <div className={`left-filters ${filtersVisible ? 'show' : 'hide'}`}>
         <div className="filters">
           <div className="search-bar">
             <input
@@ -237,7 +223,7 @@ const CsvReader = () => {
           <FieldFilter />
         </div>
       </div>
-      <div className={`right-content ${filtersVisible ? "show" : "hide"}`}>
+      <div className={`right-content ${filtersVisible ? 'show' : 'hide'}`}>
         <div className="paper-container">
           {sortedPapers.length > 0 ? (
             sortedPapers.map((row, index) => (
@@ -248,16 +234,7 @@ const CsvReader = () => {
                   <span className="year">{row.Year}</span>
                 </div>
                 <h2 className="name">
-                  <a
-                    href={row.Link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{
-                      color: "black",
-                      fontWeight: "bold",
-                      textDecoration: "none",
-                    }}
-                  >
+                  <a href={row.Link} target="_blank" rel="noopener noreferrer" style={{ color: 'black', fontWeight: 'bold',textDecoration: 'none' }}>
                     {row.Title}
                   </a>
                 </h2>
@@ -265,7 +242,7 @@ const CsvReader = () => {
                   {Object.keys(row)
                     .slice(5) // Assuming authors start from index 4 in your data
                     .map((authorKey) => row[authorKey])
-                    .join(", ")}
+                    .join(', ')}
                 </p>
               </div>
             ))
@@ -286,8 +263,8 @@ const SortFilter = ({ sortOrder, setSortOrder }) => (
         type="radio"
         id="sort-newest"
         value="desc"
-        checked={sortOrder === "desc"}
-        onChange={() => setSortOrder("desc")}
+        checked={sortOrder === 'desc'}
+        onChange={() => setSortOrder('desc')}
       />
       Newest
       <span className="radio-mark"></span>
@@ -297,8 +274,8 @@ const SortFilter = ({ sortOrder, setSortOrder }) => (
         type="radio"
         id="sort-oldest"
         value="asc"
-        checked={sortOrder === "asc"}
-        onChange={() => setSortOrder("asc")}
+        checked={sortOrder === 'asc'}
+        onChange={() => setSortOrder('asc')}
       />
       Oldest
       <span className="radio-mark"></span>
