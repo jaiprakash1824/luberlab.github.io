b4754c3d6e94967d86bc6e7971a65dc6f6eff848
amitgupta0220 <amitgupta2820@gmail.com>
2024-08-20 22:13:45 -0500

    pulled

--- src/Components/Publications/CsvReader.jsx
+++ src/Components/Publications/CsvReader.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-import './research.css';
-import SortFilter from './SortFilter';
-import TypeFilter from './TypeFilter';
-import YearFilter from './YearFilter';
-import FieldFilter from './FieldFilter';
+import { useState, useEffect } from "react";
+import Papa from "papaparse";
+import "./research.css";
+import SortFilter from "./SortFilter";
+import TypeFilter from "./TypeFilter";
+import YearFilter from "./YearFilter";
+import FieldFilter from "./FieldFilter";
 
 const baseURL = import.meta.env.BASE_URL;
 
 const CsvReader = () => {
   const [data, setData] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [sortOrder, setSortOrder] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
   const [selectedField, setSelectedField] = useState([]);
   const [selectedYears, setSelectedYears] = useState([]);
   const [selectedType, setSelectedType] = useState([]);
@@ -36,7 +36,7 @@ const CsvReader = () => {
         });
       });
 
-    const mediaQuery = window.matchMedia('(max-width: 426px)');
+    const mediaQuery = window.matchMedia("(max-width: 426px)");
     setFiltersVisible(!mediaQuery.matches); // true for desktop, false for mobile
 
     // Add event listener for changes in screen size
@@ -44,11 +44,11 @@ const CsvReader = () => {
       setFiltersVisible(!mediaQuery.matches); // true for desktop, false for mobile
     };
 
-    mediaQuery.addEventListener('change', handleResize);
+    mediaQuery.addEventListener("change", handleResize);
 
     // Clean up event listener on component unmount
     return () => {
-      mediaQuery.removeEventListener('change', handleResize);
+      mediaQuery.removeEventListener("change", handleResize);
     };
   }, []);
 
@@ -64,15 +64,23 @@ const CsvReader = () => {
   };
 
   const filteredPapers = data
-    .filter((row) => row.Title.toLowerCase().includes(searchQuery.toLowerCase()))
-    .filter((row) => (selectedField.length > 0 ? selectedField.includes(row.Field) : true))
-    .filter((row) => (selectedYears.length > 0 ? selectedYears.includes(row.Year) : true))
-    .filter((row) => (selectedType.length > 0 ? selectedType.includes(row.Paper) : true));
+    .filter((row) =>
+      row.Title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .filter((row) =>
+      selectedField.length > 0 ? selectedField.includes(row.Field) : true
+    )
+    .filter((row) =>
+      selectedYears.length > 0 ? selectedYears.includes(row.Year) : true
+    )
+    .filter((row) =>
+      selectedType.length > 0 ? selectedType.includes(row.Paper) : true
+    );
 
   const sortedPapers = [...filteredPapers].sort((a, b) => {
-    if (sortOrder === 'asc') {
+    if (sortOrder === "asc") {
       return a.Year.localeCompare(b.Year);
-    } else if (sortOrder === 'desc') {
+    } else if (sortOrder === "desc") {
       return b.Year.localeCompare(a.Year);
     } else {
       return 0; // No sorting applied
@@ -82,53 +90,89 @@ const CsvReader = () => {
   return (
     <div className="Publish-main">
       <div className="filter-toggle" onClick={toggleFiltersVisibility}>
-        {filtersVisible ? <span className="minus">&ndash; Filters</span> : <span className="plus">+ Filters</span>}
+        {filtersVisible ? (
+          <span className="minus">&ndash; Filters</span>
+        ) : (
+          <span className="plus">+ Filters</span>
+        )}
       </div>
-      <div className='manage'>
-      <div className={`left-filters ${filtersVisible ? 'show' : 'hide'}`}>
-        <div className="filters">
-          <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Search papers by title"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
+      <div className="manage">
+        <div className={`left-filters ${filtersVisible ? "show" : "hide"}`}>
+          <div className="filters">
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search papers by title"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </div>
+            <SortFilter
+              sortOrder={sortOrder}
+              setSortOrder={setSortOrder}
+              filterVisibility={filterVisibility}
+              toggleFilterSectionVisibility={toggleFilterSectionVisibility}
+            />
+            <TypeFilter
+              data={data}
+              selectedType={selectedType}
+              setSelectedType={setSelectedType}
+              filterVisibility={filterVisibility}
+              toggleFilterSectionVisibility={toggleFilterSectionVisibility}
+            />
+            <YearFilter
+              data={data}
+              selectedYears={selectedYears}
+              setSelectedYears={setSelectedYears}
+              filterVisibility={filterVisibility}
+              toggleFilterSectionVisibility={toggleFilterSectionVisibility}
+            />
+            <FieldFilter
+              data={data}
+              selectedField={selectedField}
+              setSelectedField={setSelectedField}
+              filterVisibility={filterVisibility}
+              toggleFilterSectionVisibility={toggleFilterSectionVisibility}
             />
           </div>
-          <SortFilter sortOrder={sortOrder} setSortOrder={setSortOrder} filterVisibility={filterVisibility} toggleFilterSectionVisibility={toggleFilterSectionVisibility} />
-          <TypeFilter data={data} selectedType={selectedType} setSelectedType={setSelectedType} filterVisibility={filterVisibility} toggleFilterSectionVisibility={toggleFilterSectionVisibility} />
-          <YearFilter data={data} selectedYears={selectedYears} setSelectedYears={setSelectedYears} filterVisibility={filterVisibility} toggleFilterSectionVisibility={toggleFilterSectionVisibility} />
-          <FieldFilter data={data} selectedField={selectedField} setSelectedField={setSelectedField} filterVisibility={filterVisibility} toggleFilterSectionVisibility={toggleFilterSectionVisibility} />
         </div>
-      </div>
-      <div className={`right-content ${filtersVisible ? 'show' : 'hide'}`}>
-        <div className="paper-container">
-          {sortedPapers.length > 0 ? (
-            sortedPapers.map((row, index) => (
-              <div className="paper" key={index}>
-                <div className="meta">
-                  <span className="paper-label">{row.Paper}</span>
-                  <span className="field">{row.Field}</span>
-                  <span className="year">{row.Year}</span>
+        <div className={`right-content ${filtersVisible ? "show" : "hide"}`}>
+          <div className="paper-container">
+            {sortedPapers.length > 0 ? (
+              sortedPapers.map((row, index) => (
+                <div className="paper" key={index}>
+                  <div className="meta">
+                    <span className="paper-label">{row.Paper}</span>
+                    <span className="field">{row.Field}</span>
+                    <span className="year">{row.Year}</span>
+                  </div>
+                  <h2 className="name">
+                    <a
+                      href={row.Link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{
+                        color: "black",
+                        fontWeight: "bold",
+                        textDecoration: "none",
+                      }}
+                    >
+                      {row.Title}
+                    </a>
+                  </h2>
+                  <p className="authors">
+                    {Object.keys(row)
+                      .slice(5) // Assuming authors start from index 5 in your data
+                      .map((authorKey) => row[authorKey])
+                      .join(", ")}
+                  </p>
                 </div>
-                <h2 className="name">
-                  <a href={row.Link} target="_blank" rel="noopener noreferrer" style={{ color: 'black', fontWeight: 'bold',textDecoration: 'none' }}>
-                    {row.Title}
-                  </a>
-                </h2>
-                <p className="authors">
-                  {Object.keys(row)
-                    .slice(5) // Assuming authors start from index 5 in your data
-                    .map((authorKey) => row[authorKey])
-                    .join(', ')}
-                </p>
-              </div>
-            ))
-          ) : (
-            <p>No papers found</p>
-          )}
+              ))
+            ) : (
+              <p>No papers found</p>
+            )}
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
