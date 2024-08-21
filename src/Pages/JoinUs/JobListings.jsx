56cb3d7fca9c4666b285d8ab1990f8a3e1cbdd2d
yashasvi-matta <yashasvimatta@gmail.com>
2024-08-21 19:56:59 -0400

    joinUs responisveness

--- src/Pages/JoinUs/JobListings.jsx
+++ src/Pages/JoinUs/JobListings.jsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import data from '../../../public/assets/data/jobs.json';
 import './JobListings.css';
 
-const JobListings = ({ selectedCategory }) => {
-  const jobs = selectedCategory
-    ? data.categories.find(category => category.name === selectedCategory).jobs
-    : data.categories.flatMap(category => category.jobs);
-
+const JobListings = ({ selectedJob }) => {
   return (
     <div className="job-listings">
-      {jobs.map(job => (
-        <div key={job.id} className="job-card">
+      {selectedJob ? (
+        <div key={selectedJob.id} className="job-card">
           <div className="job-header">
-            <h2>{job.title}</h2>
+            <h2>{selectedJob.title}</h2>
             <button style={{ fontWeight: 'bold' }} className="apply-button">APPLY</button>
           </div>
           <hr />
-          <p className="description">{job.description}</p>
-          {job.minimumQualification && (
-            <div className="minimum-qualification">
-              <h5>Minimum Qualification</h5>
-              <ul>
-                {job.minimumQualification.map((qual, index) => (
-                  <li key={index}>{qual}</li>
-                ))}
-              </ul>
-            </div>
-          )}
+          <p className="description">{selectedJob.description}</p>
+          <div className="minimum-qualification">
+            <h5>Minimum Qualification</h5>
+            <ul>
+              {selectedJob.minimumQualification.map((qual, index) => (
+                <li key={index}>{qual}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-      ))}
+      ) : (
+        <p>Select a job to see more details.</p>
+      )}
     </div>
   );
 };
--- src/Pages/JoinUs/JobSidebar.jsx
+++ src/Pages/JoinUs/JobSidebar.jsx
@@ -1,45 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Drawer, Button, Menu } from 'antd';
+import {MenuOutlined, CloseOutlined} from '@ant-design/icons';
 import './JobSidebar.css';
-import data from '../../../public/assets/data/jobs.json';
+import data from '../../../public/assets/data/jobs2.json';
 
-const JobSidebar = ({ setSelectedCategory, selectedCategory }) => {
+const JobSidebar = ({ setSelectedJob, selectedJob }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  const { categories } = data;
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [mobileDrawerVisible, setMobileDrawerVisible] = useState(false);
+  const jobs = data.jobs;
 
-  const filteredCategories = searchTerm
-    ? categories.filter(category =>
-        category.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : categories;
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsMobile = window.innerWidth <= 768;
+      setIsMobile(newIsMobile);
+      if (!newIsMobile) {
+        setMobileDrawerVisible(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  const handleCategoryClick = category => {
-    if (selectedCategory === category) {
-      setSelectedCategory(''); 
-    } else {
-      setSelectedCategory(category);
-    }
+  const filteredJobs = searchTerm
+    ? jobs.filter(job => job.title.toLowerCase().includes(searchTerm.toLowerCase()))
+    : jobs;
+
+  const handleJobClick = jobId => {
+    const id = parseInt(jobId, 10);
+    const job = jobs.find(job => job.id === id);
+    setSelectedJob(job);
+    if (isMobile) setMobileDrawerVisible(false);
   };
 
-  return (
-    <div className="sidebar">
+  const SidebarContent = (
+    <>
       <input
         type="text"
-        placeholder="Search categories"
+        placeholder="Search jobs"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        className="sidebarSearch"
       />
-      <ul className="category-list">
-        {filteredCategories.map((category) => (
-          <li
-            key={category.name}
-            onClick={() => handleCategoryClick(category.name)}
-            className={selectedCategory === category.name ? 'active' : ''}
-          >
-            {category.name}
-          </li>
+      <Menu
+        mode="inline"
+        selectedKeys={[selectedJob?.id?.toString()]}
+        onClick={({ key }) => handleJobClick(key)}
+      >
+        {filteredJobs.map(job => (
+          <Menu.Item key={job.id.toString()}>
+            {job.title}
+          </Menu.Item>
         ))}
-      </ul>
-    </div>
+      </Menu>
+    </>
+  );
+
+  return (
+    <>
+      {isMobile && (
+        <Button className="menuButton" type="primary" 
+        icon={mobileDrawerVisible ? <CloseOutlined style={{ color: 'white' }} /> : <MenuOutlined style={{ color: 'black' }} />}
+        onClick={() => setMobileDrawerVisible(!mobileDrawerVisible)} />
+      )}
+      {isMobile ? (
+        <Drawer
+          title="Job Listings"
+          placement="left"
+          closable={true}
+          onClose={() => setMobileDrawerVisible(false)}
+          open={mobileDrawerVisible}
+          bodyStyle={{ padding: 0 }}
+          width={250}
+        >
+          {SidebarContent}
+        </Drawer>
+      ) : (
+        <div className="desktop-sidebar">
+          {SidebarContent}
+        </div>
+      )}
+    </>
   );
 };
 
