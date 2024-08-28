110cd7147c9cd1dea40f626e837a93a650c5afc8
yashasvi-matta <yashasvimatta@gmail.com>
2024-08-28 19:21:26 -0400

    join us fixed

--- src/Pages/JoinUs/JoinUs.jsx
+++ src/Pages/JoinUs/JoinUs.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import JobSidebar from "./JobSidebar";
 import JobListings from "./JobListings";
+import data from "../../../public/assets/data/jobs2.json"; 
 import "./JoinUs.css";
 
 const JoinUs = () => {
-  const [selectedJob, setSelectedJob] = useState(null);
+
+  const [selectedJob, setSelectedJob] = useState(data.jobs[0] || null);
 
   return (
     <div className="join-us-container">
-      <JobSidebar setSelectedJob={setSelectedJob} selectedJob={selectedJob} />
+      <JobSidebar setSelectedJob={setSelectedJob} selectedJob={selectedJob} jobs={data.jobs} />
       <JobListings selectedJob={selectedJob} />
     </div>
   );
