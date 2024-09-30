e97d3535a1944a35fed2db9a82809ff2874df386
yashasvi-matta <yashasvimatta@gmail.com>
2024-09-30 13:44:03 -0400

    research page particles added

---
+++ src/Pages/Research/ResearchCarouselThree.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const ResearchCarouselThree = () => {
+    return (
+        <div className="section" id="section2">
+            <h1>Core Machine Learning & AI</h1>
+            <p>This category covers foundational AI and machine learning techniques across various domains. It includes
+Vision-Based Machine Learning
+, which focuses on applying machine learning to visual data like images and medical scans for tasks such as classification, segmentation, and diagnosis.
+Generative AI and Deep Learning
+explores the development of deep neural networks for generating synthetic data, images, and text, focusing on models like GANs and VAEs.
+Responsible AI
+emphasizes ethical considerations, fairness, and bias mitigation in AI, ensuring that models are transparent, explainable, and equitable in their predictions and decisions.</p>
+        </div>
+    )
+}
+
+export default ResearchCarouselThree;
+
---
+++ src/Pages/Research/ResearchCarouselTwo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ResearchCarouselTwo = () => {
+    return (
+        <div className="section" id="section2">
+            <h1>Human-Computer Interaction and Interactive Visualization</h1>
+            <p>This category explores the intersection of AI, machine learning, and user interfaces. It focuses on creating intuitive tools and platforms for visualizing complex datasets like genome interaction maps and other multi-dimensional data, enabling users to interact with and explore data insights. It covers web-based visualizations, immersive VR tools, and interfaces designed for both scientific and clinical applications, enhancing user engagement and decision-making through AI-driven visual analytics.</p>
+        </div>
+    );
+}
+
+export default ResearchCarouselTwo;
